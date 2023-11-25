b55a445b250e4adf9997c5a769678973a7f8a04e
Burak Ayyorgun <120507146+burakayy7@users.noreply.github.com>
2023-11-25 15:52:09 -0500

    Update burak_lsm9ds1.cpp

--- burak_lsm9ds1.cpp
+++ burak_lsm9ds1.cpp
@@ -1,1 +1,10 @@
+/***************************************************************************
+  This is a library for the LSM9DS1 Accelerometer and magnentometer/compass
 
+  Designed specifically to work with the Adafruit LSM9DS1 Breakouts with Arduino
+
+  These sensors use I2C to communicate, 2 pins are required to interface.
+
+  Written by Burak Ayyorgun
+  under the MT License
+ ***************************************************************************/
